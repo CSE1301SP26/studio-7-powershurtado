d0129725a15afef2ecee12ee15506068418ca159
mfpowers <m.f.powers@wustl.edu>
2026-03-25 14:12:32 -0500

    commitiy

--- src/studio7/Rectangle.java
+++ src/studio7/Rectangle.java
@@ -1,36 +1,41 @@
+
 public class Rectangle{
+
+
     private double length; 
     private double width; 
 
     public Rectangle(double newLength, double newWidth){ 
-        length = newLength
-        width = newWidth
+         length = newLength;
+         width = newWidth;
     }
         public double getLength(){
             return length;
         }
         public void setLength(double newLength){
-            length = newLength;
+         length = newLength;
         }
         public double getWidth(){
             return width;
         }
         public void setWidth(double newWidth){
-            width = newWidth;
+             width = newWidth;
         }
     public double getArea(){
-        return (length*width);
+        double area = length*width;
+        return area;
 
+}
     
-    }
-newsquare = new Rectangle(2.0,2.0)'
+
 
 
 }
 public static void main(String[] args){
-    System.out.println(getArea(newsquare))
+   double square = new Rectangle(2.0,2.0);
+    System.out.println(getArea(square));
 }
-        
+       
 
 
     
